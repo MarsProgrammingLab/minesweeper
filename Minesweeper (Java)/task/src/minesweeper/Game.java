1c2055fc8383975ee0c216d4a71de43ee3979461
MarsProgrammingLab <martavias.cary@outlook.com>
2024-12-29 21:54:16 -0500

    add functionality from main to game class

---
+++ Minesweeper (Java)/task/src/minesweeper/Game.java
@@ -0,0 +1,19 @@
+package minesweeper;
+
+import java.util.Scanner;
+
+public class Game {
+    private final int ROWS = 9;
+    private final int COLS = 9;
+
+
+    public void start() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("How many mines do you want on the field? ");
+        int mines = scan.nextInt();
+
+        Minefield field = new Minefield(ROWS, COLS, mines);
+        field.printField();
+    }
+}
--- Minesweeper (Java)/task/src/minesweeper/Main.java
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -1,11 +1,9 @@
 package minesweeper;
 
-import java.util.Random;
-
 public class Main {
 
     public static void main(String[] args) {
-        Minefield field = new Minefield(4, 4, 4);
-        field.printField();
+        Game game = new Game();
+        game.start();
     }
 }
